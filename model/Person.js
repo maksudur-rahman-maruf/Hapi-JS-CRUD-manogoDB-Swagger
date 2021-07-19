765841dc20d466e592539b7694a8a7976eca4966
Maksudur Rahman Maruf <43263023+maksudur-rahman-maruf@users.noreply.github.com>
2021-07-19 20:21:06 +0600

    Delete model directory

--- model/Person.js
+++
@@ -1,9 +0,0 @@
-const Mongoose = require("mongoose");
-const Schema = Mongoose.Schema;
-
-const PersonModel = Mongoose.model("person", {
-    firstname: String,
-    lastname: String
-});
-
-module.exports = PersonModel;
